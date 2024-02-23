e6ad7bc00e4df9c91f3aed9f57a3008b4cea9076
mohammadasif2808 <mohammadasif2808@gmail.com>
2024-02-23 12:12:29 +0530

    Initial commit

---
+++ readme.md
@@ -0,0 +1,2 @@
+### Json Schema To Pojo Convertor
+[https://www.jsonschema2pojo.org/](https://www.jsonschema2pojo.org/)
---
+++ src/main/java/in/co/mohammadasif/DevVaultApplication.java
@@ -0,0 +1,13 @@
+package in.co.mohammadasif;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DevVaultApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DevVaultApplication.class, args);
+	}
+
+}
